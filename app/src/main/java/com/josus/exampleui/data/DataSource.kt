7f1ed66e064db60d0ed8cc015f5eed0c816558e7
josus praiser <josus.praise@gmail.com>
2021-08-22 16:22:43 +0530

    Example UI v3

--- app/src/main/java/com/josus/exampleui/data/DataSource.kt
+++ app/src/main/java/com/josus/exampleui/data/DataSource.kt
@@ -8,17 +8,17 @@ class DataSource {
 
     fun loadFirstList(): List<FirstList> {
         return listOf(
-            FirstList(R.drawable.ic_seacrh, R.drawable.button_bg , R.string.title_1, R.string.sub_title),
-            FirstList(R.drawable.ic_seacrh, R.drawable.button_bg, R.string.title_1, R.string.sub_title),
-            FirstList(R.drawable.ic_seacrh,R.drawable.button_bg,R.string.title_1,R.string.sub_title),
-            FirstList(R.drawable.ic_seacrh,R.drawable.button_bg,R.string.title_1,R.string.sub_title),
-            FirstList(R.drawable.ic_seacrh,R.color.black,R.string.title_1,R.string.sub_title),
-            FirstList(R.drawable.ic_seacrh,R.color.black,R.string.title_1,R.string.sub_title),
-            FirstList(R.drawable.ic_seacrh,R.color.black,R.string.title_1,R.string.sub_title),
-            FirstList(R.drawable.ic_seacrh,R.color.black,R.string.title_1,R.string.sub_title),
-            FirstList(R.drawable.ic_seacrh,R.color.black,R.string.title_1,R.string.sub_title),
-            FirstList(R.drawable.ic_seacrh,R.color.black,R.string.title_1,R.string.sub_title),
-            FirstList(R.drawable.ic_seacrh,R.color.black,R.string.title_1,R.string.sub_title)
+            FirstList(R.drawable.star_icon, R.drawable.btn_bg_1, R.string.title_1, R.string.sub_title),
+            FirstList(R.drawable.qrcode_icon, R.drawable.btn_bg_2, R.string.title_2, R.string.sub_title),
+            FirstList(R.drawable.new_compliance,R.drawable.btn_bg_3,R.string.title_3,R.string.sub_title),
+            FirstList(R.drawable.invoice_icon,R.drawable.btn_bg_4,R.string.title_4,R.string.sub_title),
+            FirstList(R.drawable.ic_seacrh,R.drawable.btn_bg_1,R.string.title_1,R.string.sub_title),
+            FirstList(R.drawable.ic_seacrh, R.drawable.btn_bg_2,R.string.title_1,R.string.sub_title),
+            FirstList(R.drawable.ic_seacrh,R.drawable.btn_bg_3,R.string.title_1,R.string.sub_title),
+            FirstList(R.drawable.ic_seacrh,R.drawable.btn_bg_4,R.string.title_1,R.string.sub_title),
+            FirstList(R.drawable.ic_seacrh,R.drawable.btn_bg_2,R.string.title_1,R.string.sub_title),
+            FirstList(R.drawable.ic_seacrh,R.drawable.btn_bg_4,R.string.title_1,R.string.sub_title),
+            FirstList(R.drawable.ic_seacrh,R.drawable.btn_bg_1,R.string.title_1,R.string.sub_title)
             )
     }
 
